4ac25cb7d41a3b5ef473009bde1a26b771474f19
cktarun <118742506+cktarun@users.noreply.github.com>
2024-02-20 12:38:22 +0000

    container config done

--- 01-basics/02_dataTypes.js
+++ 01-basics/02_dataTypes.js
@@ -2,3 +2,17 @@
 
 // alert(3+3) // we are using nodejs,not browser
 
+// TO SAVE FILES IN GITHUB WE USE container config done.. 
+
+// number : 2 to power 53
+// bigint (use for bigger values)\
+// string : ""
+// boolean : true/false
+// null : standalone value
+// undefined : value not assign
+// symbol : unique
+
+
+// object
+console.log(typeof null);
+console.log(typeof undefined);
