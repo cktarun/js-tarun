1799d32ee64c961c2fb410891f77faf7837267da
cktarun <118742506+cktarun@users.noreply.github.com>
2024-02-20 13:12:27 +0000

    conversion of datatype

---
+++ 01-basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let run = 50;
+
+console.log(typeof run);
+console.log(typeof (run));
+
+let score = Number(run)
+console.log(typeof score);
+console.log(score);
+
+// "50" : 50
+// "50xyz" : NaN
+// true : 1
+// false : 0
+
+let isLoggedIn = 1
+let s = Boolean(isLoggedIn);
+console.log(s)
+
+// 1 : true
+// "" : false
+// "ck" : true
+
+let num = 30
+ let hum = String(num);
+ console.log(hum);
+ console.log(typeof hum);
